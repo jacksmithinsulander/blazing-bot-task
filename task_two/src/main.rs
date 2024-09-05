8fec30dd203946a78fe32a3414dc9e1fa7765448
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-09-05 15:24:47 +0200

    final

--- task_two/src/main.rs
+++ task_two/src/main.rs
@@ -1,12 +1,12 @@
 use alloy::{
-    eips::BlockId, network::TransactionBuilder, node_bindings::{Anvil, AnvilInstance}, primitives::{
-        address, keccak256, Address, FixedBytes, Uint, U256
+    eips::BlockId, network::TransactionBuilder, node_bindings::Anvil, primitives::{
+        keccak256, Address, FixedBytes, Uint, U256
     }, providers::{
         ext::{AnvilApi, DebugApi, TraceApi}, Provider, ProviderBuilder, RootProvider
     }, rpc::types::{
         trace::geth::{
             CallFrame, GethDebugBuiltInTracerType, GethDebugTracerType, GethDebugTracingOptions, GethDefaultTracingOptions
-        }, Transaction, TransactionReceipt, TransactionRequest
+        }, Transaction, TransactionRequest
     }, sol, transports::http::{Client, Http}
 };
 use eyre::Result;
@@ -53,22 +53,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let tx = provider.get_transaction_by_hash(tx_hash).await?.unwrap();
 
-    let receipt = provider.get_transaction_receipt(tx_hash).await?.unwrap();
-
     let anvil_partial = Anvil::new().fork("https://eth-pokt.nodies.app").fork_block_number(tx.block_number.unwrap()-1).try_spawn()?;
     let rpc_url_partial = anvil_partial.endpoint().parse()?;
     let provider_partial = ProviderBuilder::new().on_http(rpc_url_partial);
 
-    trace_block(tx_hash, tx.block_number.unwrap(), provider, provider_partial /*, anvil, receipt */).await?;
-
-    //match Action::from_str(&action_input) {
-        //Ok(Action::Buy) => buy_info(tx_hash, provider, provider_partial).await?,
-        //Ok(Action::Sell) => sell_info(tx_hash, provider, provider_partial).await?,
-        //Err(_) => {
-            //println!("Invalid action! Please enter either 'buy' or 'sell'.");
-            //return Ok(());
-        //}
-    //}
+    match Action::from_str(&action_input) {
+        Ok(Action::Buy) => buy_info(tx_hash, provider, provider_partial, tx).await?,
+        Ok(Action::Sell) => sell_info(tx_hash, provider, provider_partial, tx).await?,
+        Err(_) => {
+            println!("Invalid action! Please enter either 'buy' or 'sell'.");
+            return Ok(());
+        }
+    }
 
     Ok(())
 }
@@ -101,21 +97,8 @@ fn prompt_user(prompt: &str) -> String {
     input.trim().to_string()
 }
 
-async fn fetch_transaction_details(
-    tx_hash: FixedBytes<32>,
-) -> Result<(RootProvider<Http<Client>>, Transaction, TransactionReceipt)> {
-    let rpc_url = "https://eth-pokt.nodies.app".parse()?;
-
-    let provider = ProviderBuilder::new().on_http(rpc_url);
-
-    let tx = provider.get_transaction_by_hash(tx_hash).await?.unwrap();
+async fn sell_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>, provider_partial: RootProvider<Http<Client>>, tx: Transaction) -> Result<()>  {
     let receipt = provider.get_transaction_receipt(tx_hash).await?.unwrap();
-
-    Ok((provider, tx, receipt))
-}
-
-async fn sell_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>, provider_partial: RootProvider<Http<Client>>) -> Result<()>  {
-    let (provider_, tx, receipt) = fetch_transaction_details(tx_hash).await?;
 
     let included_block = tx.block_number.unwrap();
     let from = tx.from;
@@ -133,14 +116,6 @@ async fn sell_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>
                 let token_address = log.inner.address;
 
                 println!("Sent token address: {}", token_address);
-                let contract = ERC20::new(token_address, &provider);
-
-                let decimals = contract.decimals().call().await?._0;
-
-                let ticker = contract.symbol().call().await?._0;
-
-                let pre = contract.balanceOf(from).block(BlockId::number(included_block - 1)).call().await?.balance;
-                let post = contract.balanceOf(from).block(BlockId::number(included_block + 1)).call().await?.balance;
 
                 let call_options = GethDebugTracingOptions {
                     config: GethDefaultTracingOptions {
@@ -156,6 +131,8 @@ async fn sell_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>
 
                 let sum_eth = sum_calls(&result, receipt.from);
 
+                let (pre, post, decimals, ticker) = get_token_info(tx_hash, included_block, provider.clone(), provider_partial, sender_address, token_address).await?;
+
                 let amount_token = format_with_padding(pre - post, decimals as usize);
                 
                 let amount_eth = format_with_padding(U256::from(sum_eth), 18);
@@ -170,8 +147,8 @@ async fn sell_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>
     Ok(())
 }
 
-async fn buy_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>, provider_partial: RootProvider<Http<Client>>) -> Result<()> {
-    let (provider_, tx, receipt) = fetch_transaction_details(tx_hash).await?;
+async fn buy_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>, provider_partial: RootProvider<Http<Client>>, tx: Transaction) -> Result<()> {
+    let receipt = provider.get_transaction_receipt(tx_hash).await?.unwrap();
 
     let included_block = tx.block_number.unwrap();
     let from = tx.from;
@@ -190,14 +167,7 @@ async fn buy_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>,
                 let token_address = log.inner.address;
                 println!("Received token address = {}", token_address);
 
-                let contract = ERC20::new(token_address, &provider);
-
-                let decimals = contract.decimals().call().await?._0;
-
-                let ticker = contract.symbol().call().await?._0;
-
-                let pre = contract.balanceOf(from).block(BlockId::number(included_block - 1)).call().await?.balance;
-                let post = contract.balanceOf(from).block(BlockId::number(included_block + 1)).call().await?.balance;
+                let (pre, post, decimals, ticker) = get_token_info(tx_hash, included_block, provider, provider_partial, receiver_address, token_address).await?;
 
                 let amount_sent = format_with_padding(post - pre, decimals as usize);
 
@@ -211,78 +181,51 @@ async fn buy_info(tx_hash: FixedBytes<32>, provider: RootProvider<Http<Client>>,
     Ok(())
 }
 
-async fn trace_block(
+async fn get_token_info(
     tx_hash: FixedBytes<32>, 
     included_block: u64, 
     provider: RootProvider<Http<Client>>,
-    provider_partial: RootProvider<Http<Client>> 
-) -> Result<(Uint<256, 4>, Uint<256, 4>)> {
+    provider_partial: RootProvider<Http<Client>>,
+    user_address: Address,
+    token_address: Address 
+) -> Result<(Uint<256, 4>, Uint<256, 4>, u8, String)> {
     let block_trace = provider.trace_block(BlockId::number(included_block)).await?;
 
+    provider_partial.anvil_auto_impersonate_account(true).await?;
+    provider_partial.anvil_impersonate_account(user_address).await?;
 
-    let call_options = GethDebugTracingOptions {
-            config: GethDefaultTracingOptions {
-                disable_storage: Some(true),
-                enable_memory: Some(false),
-                ..Default::default()
-            },
-            tracer: Some(GethDebugTracerType::BuiltInTracer(GethDebugBuiltInTracerType::CallTracer)),
-            ..Default::default()
-        };
+    let contract = ERC20::new(token_address, provider_partial.clone());
+    let decimals = contract.decimals().call().await?._0;
+    let ticker = contract.symbol().call().await?._0;
 
     let mut bal_pre= U256::from(0);
     let mut bal_post = U256::from(0);
+
     for transaction in block_trace.iter() {
-        let s = transaction.transaction_hash.unwrap();
-        let q = transaction.transaction_position.unwrap();
+        let s = transaction.transaction_hash;
+        let result_pre = transaction.trace.action.as_call();
 
-        let result = provider.debug_trace_transaction(s, call_options.clone()).await?.try_into_call_frame().unwrap();
-        let w = result.input;
+        if let (Some(s), Some(result)) = (s, result_pre) {
+            if s == tx_hash {
+                bal_pre = contract.balanceOf(result.from).call().await?.balance;
 
-        provider_partial.anvil_auto_impersonate_account(true).await?;
-        provider_partial.anvil_impersonate_account(result.from).await?;
+                let input = result.input.clone();
 
-        if s == tx_hash {
-            let o = transaction;
-            println!("FOUND IT");
+                let new_transaction = TransactionRequest::default()
+                    .with_from(result.from)
+                    .with_to(result.to)
+                    .with_value(result.value)
+                    .with_input(input);
 
-            let ctrct = ERC20::new(address!("ef00a1910642520c2F8e23d3C0A910933ca7f358"), provider_partial.clone());
+                provider_partial.send_transaction(new_transaction).await?.get_receipt().await?;
 
-            bal_pre= ctrct.balanceOf(result.from).call().await?.balance;
-            print!("{o:?}");
-            let new_transaction = TransactionRequest::default()
-                .with_from(result.from)
-                .with_to(result.to.unwrap())
-                .with_value(result.value.unwrap())
-                .with_input(w);
-
-            let new_pending = provider_partial.send_transaction(new_transaction).await?;
-
-            println!("Pending transaction... {}", new_pending.tx_hash());
-
-            let new_receipt = new_pending.get_receipt().await?;
-
-            println!(
-                "Transaction included in block {}",
-                new_receipt.block_number.expect("Failed to get block number")
-            );
-            bal_post = ctrct.balanceOf(result.from).call().await?.balance;
-
-            println!("Before: {} After: {}", bal_pre, bal_post);
-
-            let difference = bal_post - bal_pre;
-
-            let decimals = ctrct.decimals().call().await?._0;
-            let formatted_bought = format_with_padding(difference, decimals as usize);
-            println!("Actual bought amount: {}", formatted_bought);
-
-            break;
+                bal_post = contract.balanceOf(result.from).call().await?.balance;
+                break;
+            }
         }
-
     }
 
-
-    Ok((bal_pre, bal_post))
+    Ok((bal_pre, bal_post, decimals, ticker))
 }
 
 fn format_with_padding(value: U256, decimals: usize) -> String {
